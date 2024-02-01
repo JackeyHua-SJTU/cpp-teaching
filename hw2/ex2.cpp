686e8e952adf401b2fded8dee43376760e165991
Jackey Hua <dyhzd2013@163.com>
2024-02-01 21:28:38 +0800

    feat: hw2 release

---
+++ hw2/ex2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //* 让用户输入一个整数，判断这个数是不是回文数，如12321是回文数，1232不是回文数
+    //* Hint: 可以用一个数组存储整数的每一位
+    // TODO: Write your code here
+    
+    return 0;
+}
---
+++ hw2/ex3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //* 输入一句英语句子，输出同样的句子但是删除所有空格。如输入"This is an apple."，则输出"Thisisanapple."
+    //* 假设输入句子的长度不超过50个字符，即可以定义一个大小为50的字符数组
+    // TODO: Write your code here
+    
+    return 0;
+}
